9f2e42373e5c5ab58410e4ba59ec0d4fe4a1d320
Uncle_Iroh <vadegtyarev@gmail.com>
2022-09-04 16:24:42 +0300

    Fist lesson

---
+++ Lesson_1.py
@@ -0,0 +1,58 @@
+# print('Hellow word')
+
+# a = 123
+# b = 1.23
+
+# print(a)
+# print(b)
+
+#s = 'yes'
+
+# print(s)
+
+# print('Введите а');
+#a = float(input())
+#print('Введите b');
+#b = float(input())
+# print(a, b, s)
+# print('{} - {} - {}' .format(a, b, s)) 
+# # вывод чере формат и в таком шаблоне выводятся по порядку (если в {} поставить индекс числа от 0 то можно задать порядок вывода)
+# print(f'{a}, {b}, {s}') 
+# # вывод через интерполяцию
+
+# f = True
+# g = False
+# print(f)
+# print(g)
+
+#list = ['1', '2', 'yes']
+#print(list)
+
+#print(a, ' + ',  b, ' = ', a+b)
+
+#f = [1,2,3,4]
+#print(f)
+#print(not 5 in f)
+
+#is_odd = f[0] % 2 == 0
+#print(is_odd)
+#is_odd = not f[0] % 2
+#print(is_odd)
+ 
+#a = int(input('a = '))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
+
+username = input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это же МАША!')
+elif username == 'Марина':
+    print('Я так ждала Вас, Марина!')
+elif username == 'Ильнар':
+    print('Ильнар - топ)')
+else:
+    print('Привет, ', username)
+ 
